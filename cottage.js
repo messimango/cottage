dda709296803d56d4539cb52140b2c6302a079c4
Umang Poudyal <umangpoudyal@gmail.com>
2022-03-11 23:51:06 -0500

    first commit

---
+++ cottage.js
@@ -0,0 +1,7 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const menuItems = document.querySelector(".menu-items");
+
+
+menuToggle.addEventListener("click", function () {
+    menuItems.classList.toggle("show-links");
+});
