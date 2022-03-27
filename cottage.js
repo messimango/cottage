72be02b425d76feeec5951779099ec0accf94e5b
Umang Poudyal <umangpoudyal@gmail.com>
2022-03-27 17:15:59 -0400

    updates

--- cottage.js
+++ cottage.js
@@ -1,3 +1,5 @@
+// toggle nav
+
 const menuToggle = document.querySelector(".menu-toggle");
 const menuItems = document.querySelector(".menu-items");
 
@@ -5,3 +7,24 @@ const menuItems = document.querySelector(".menu-items");
 menuToggle.addEventListener("click", function () {
     menuItems.classList.toggle("show-links");
 });
+
+
+// check in date
+
+const startDate = document.getElementById('start')
+
+startDate.min = new Date().toLocaleDateString('en-ca');
+
+
+// stick navbar
+
+var navbar = document.querySelector(".nav-bar");
+
+window.addEventListener('scroll', function () {
+    if (this.window.pageYOffset > 70) {
+        navbar.classList.add('hover')
+    } else {
+        navbar.classList.remove('hover')
+    }
+    
+})
